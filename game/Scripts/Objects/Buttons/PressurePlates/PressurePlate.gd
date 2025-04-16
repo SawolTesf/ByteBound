946a0e5afac8af697032c8e9af0f3f354fefaf26
Jachin Minyard <jachinminyard@gmail.com>
2025-04-16 10:41:30 -0500

    Made the ball interact with the pressure plate

--- game/Scripts/Objects/Buttons/PressurePlates/PressurePlate.gd
+++ game/Scripts/Objects/Buttons/PressurePlates/PressurePlate.gd
@@ -1,5 +1,5 @@
 class_name PressurePlate extends TriggerButton
-## A pressure plate only deactivates a lazer while there is something on top of it.operator !=
+## A pressure plate only deactivates a lazer while there is something on top of it.
 ##
 ## Checks for collisions with different types of objects.
 ## This allows for things like the player to turn off a lazer by standing on it or by placing a box on it.
@@ -12,17 +12,35 @@ func _ready() -> void:
 
 	# connect the signal
 	body_exited.connect(_on_body_exited)
+	body_entered.connect(_on_body_entered)
+	
 
 	#plateStepped = get_node("PlatePressed")
 
+func _on_body_entered(body: Node2D) -> void:
+	Debug.debug(self, "%s Entered the buttons Area2D" % body.name, false)
+	if body.is_in_group("Player") or body.is_in_group("Throwable"):
+		# Only emit the signal if the button is not already activated
+		# Keeps the signal from emiting more then once per entering.
+		if not is_activated:
+			# Play the transition animation (button being pressed)
+			sprite.play("Activate")
+			light.enabled = false
+			
+			# Set before the signal is emitted
+			# Makes sure that on entering the activated signal is emitted
+			is_activated = true
+
+			# emit the proper signal
+			signal_emiter()
 
 func _on_body_exited(body: Node2D) -> void:
 	Debug.debug(self, "%s Left the area of the pressure plate" % body.get_script().get_global_name(), false)
-	if body.is_in_group("Player"):
+	if body.is_in_group("Player") or body.is_in_group("Throwable"):
 		# Play the transition animation (button being pressed)
 		sprite.play("Deactivate")
 		
-		# set is active to flase before the signal is emitted
+		# set is active to false before the signal is emitted
 		# Makes sure that on leaving the deactivated signal is emitted 
 		is_activated = false
 		signal_emiter()
