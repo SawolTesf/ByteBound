602b5415f25c4e0001c0cccdf44c1dc7274dfb65
Brian Pham <frostypqnda@gmail.com>
2025-04-29 13:31:03 -0500

    feat(Added camera panning for level 1):

--- game/Scenes/Levels/panning_camera.gd
+++ game/Scenes/Levels/panning_camera.gd
@@ -1,18 +1,18 @@
 extends Camera2D
 
 var init_activated: bool = false
-@onready var lazer = $"../Node/BlueLazer"
+@onready var player_cam = $"../Characters/Player".camera
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	SignalHub.blue_pedistal_activated.connect(_pan_camera)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 func _pan_camera():
 	if !init_activated:	
 		init_activated = true
-		position = lazer.position
-		print(position)
+		if player_cam.is_current():
+			make_current()
+		else:
+			player_cam.make_current()
+
+		
