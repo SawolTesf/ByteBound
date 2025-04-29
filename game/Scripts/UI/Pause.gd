d63629086dc06cf50d8d1ea4371a75e565aecd21
Jachin Minyard <jachinminyard@gmail.com>
2025-04-29 11:26:11 -0500

    IDK level changes?

--- game/Scripts/UI/Pause.gd
+++ game/Scripts/UI/Pause.gd
@@ -2,7 +2,6 @@ extends CanvasLayer
 @onready var reload_button: Button = %Reload
 @onready var main_menu_button: Button = %MainMenu
 @onready var quit_button: Button = %Quit
-@onready var settings_button: Button = %Settings
 
 func _ready():
 	hide()
@@ -10,7 +9,6 @@ func _ready():
 	main_menu_button.pressed.connect(SceneManager.open_main_menu)
 	reload_button.pressed.connect(SceneManager.reload)
 	quit_button.pressed.connect(quit_game)
-	settings_button.pressed.connect(SceneManager.open_settings_menu)
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
@@ -23,4 +21,3 @@ func toggle_pause():
 
 func quit_game():
 	get_tree().quit()
-
